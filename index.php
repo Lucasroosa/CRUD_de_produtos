d5dea09fcdbe0693d1c3d38b7944c3e573bfac53
Lucasroosa <121141787+Lucasroosa@users.noreply.github.com>
2022-12-20 23:56:17 -0300

    Update index.php
    Ajustando css

--- index.php
+++ index.php
@@ -8,7 +8,21 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="style.css">
+    <!---<link rel="stylesheet" href="style.css">-->
+    
+        <style type="text/css">
+
+        #tamanhoContainer {
+            width: 500px;
+        }
+        #botao {
+            background-color: #00AEBC;
+            color: #ffffff;
+            font-weight: bold;
+        }
+
+
+    </style>
    
 
     
